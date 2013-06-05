a622409b55fd0c61834598cc6c192581a3104272
NicNLD <nic-nld@live.nl>
2013-06-05 17:00:15 +0200

    Added English and Dutch translation files.

---
+++ i18n/en-message.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Page Image
+ * 
+ * The page image plugin lets you associate an image with a page.
+ * 
+ * @package     Plugins
+ * @subpackage  page_image
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2013
+ * @version     0.1.0
+ */
+
+return array(
+    'Page Image' => 'Page Image', 
+    'The page image plugin lets you associate an image with a page.' => 'The page image plugin lets you associate an image with a page.', 
+    'Image' => 'Image', 
+    'Settings' => 'Settings', 
+    'None' => 'None', 
+);
---
+++ i18n/nl-message.php
@@ -0,0 +1,23 @@
+<?php
+if (!defined('IN_CMS')) { exit(); }
+
+/**
+ * Page Image
+ * 
+ * The page image plugin lets you associate an image with a page.
+ * 
+ * @package     Plugins
+ * @subpackage  page_image
+ * 
+ * @author      Nic Wortel <nic.wortel@nth-root.nl>
+ * @copyright   Nic Wortel, 2013
+ * @version     0.1.0
+ */
+
+return array(
+    'Page Image' => 'Pagina-afbeelding', 
+    'The page image plugin lets you associate an image with a page.' => 'De pagina-afbeelding plugin biedt de mogelijkheid om een afbeelding aan een pagina te koppelen.', 
+    'Image' => 'Afbeelding', 
+    'Settings' => 'Instellingen', 
+    'None' => 'Geen', 
+);
